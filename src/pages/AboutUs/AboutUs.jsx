4a8fefe74589944fd242786317dab7f98b94a11c
AlfaCefOf <alfaxcef@gmail.com>
2025-09-21 10:46:38 +0500

    first commit

--- src/pages/AboutUs/AboutUs.jsx
+++ src/pages/AboutUs/AboutUs.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-import './spg.css'
+import React from "react";
+import "./spg.css";
+import wallet from "../AboutUs/img/wallet.jpg";
+
 function spg() {
   return (
-    <div>spg</div>
-  )
+    <>
+      <div>
+        <h1>About Us</h1>
+        <p>
+          At Lost & Found, we know how stressful it can be to lose something
+          important. Whether it’s a wallet, a phone, or a sentimental keepsake,
+          losing an item often means losing peace of mind. That’s why we built
+          this platform: to give people an easy, safe, and reliable way to
+          reconnect with their lost belongings.
+        </p>
+        <h2>Our mission</h2>
+        <img src={wallet} alt="" />
+      </div>
+    </>
+  );
 }
 
-export default spg
+export default spg;
