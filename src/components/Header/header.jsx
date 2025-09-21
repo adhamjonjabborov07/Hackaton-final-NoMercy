f23fdc9d81ce1397cf7e9becf1ed1439a27e7897
adhamjon0770 <isagi3572@gmail.com>
2025-09-21 11:06:26 +0500

    dinfsdinfisdnfi

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./header.css"; // style yozib olishing uchun
-
+import "./header.css"; 
+import Logo from "../Header/Group 46.png";
 function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/logo.png" alt="I Found Logo" className="logo-img" />
-        <span className="logo-text">I-Found</span>
+        <img src={Logo} alt="I Found Logo" className="logo-img" />
+       <div className="second-logo">
+         <span className="logo-text">I-Found</span>
         <small className="slogan">Search · Connect · Recover</small>
+       </div>
       </div>
 
       <nav className="nav">
