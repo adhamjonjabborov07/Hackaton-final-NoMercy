ae7fce638fe33f12aa240258dd333bfdf2178f06
adhamjon0770 <isagi3572@gmail.com>
2025-09-21 09:35:26 +0500

    header qoshildi

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-function header() {
+import React from "react";
+import { Link } from "react-router-dom";
+function Header() {
   return (
-    <div>header</div>
-  )
+    <header>
+      <div>
+        <Link to={"/"}>Home</Link>
+      </div>
+
+      <nav className="nav">
+        <Link className="pages-style" to="/Secondpage">
+          SecondPage
+        </Link>
+        <Link className="pages-style" to="/Thirdpage">
+          Thirdage
+        </Link>
+      </nav>
+
+      {/* <div className="right">
+        <Link to="/login" className="btn btn-primary">
+          Sign In
+        </Link>
+        <Link to="/signUp" className="btn btn-primary">
+          Sign Up
+        </Link>
+      </div> */}
+    </header>
+  );
 }
 
-export default header
+export default Header;
